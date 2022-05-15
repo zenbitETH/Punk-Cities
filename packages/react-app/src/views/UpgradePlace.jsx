9a2004e216a7e2007499b24af69955b3d52fcd55
ale_maci <alessandromaci96@gmail.com>
2022-05-15 20:26:35 +0200

    contract update

--- packages/react-app/src/views/UpgradePlace.jsx
+++ packages/react-app/src/views/UpgradePlace.jsx
@@ -7,7 +7,7 @@ const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const web3 = createAlchemyWeb3(alchemyKey);
 
-const contractAddressLocal = "0x092BBe9022D421940B6D74799179267e5c822895"; // to find a better way to retrieve this address
+const contractAddressLocal = "0x20B991Dcf13f0854c142B4A5F217cBCD640e6409"; // to find a better way to retrieve this address
 const contractInstance = new web3.eth.Contract(PunkCityABI, contractAddressLocal);
 
 export default function Upgrade({ address }) {
