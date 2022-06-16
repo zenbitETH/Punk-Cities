de0771a4f1fe3d26d3c9bcea9c8929e6f46d2773
HabacucMX <habacuc.vera@gmail.com>
2022-06-16 13:23:14 -0500

    Homescreen spanish

--- packages/react-app/src/views/HomeScreen.jsx
+++ packages/react-app/src/views/HomeScreen.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
+import PC from "../assets/punkcities.png"
 
 export default function HomeScreen() {
 
 
   return (
-    <div class="HomeCon">
-      <img class="HomeScreen"src="https://punkcities.mypinata.cloud/ipfs/QmPoSnaj68Lcbs8TiAT1Lg9aodWcXE27t94kjhAw8xYZwn"/>
-      <br/>
-      <a class="NewGameBt" href="./NewGame">
-        Start New Game
-      </a>
+    <div className="HomeCon">
+      <main className="HomeCon2">
+        <div className="hero">
+          <div className="center"><img src={PC} class="logo" alt="Punk Cities"/></div>
+          
+            <div className="HomeBT">Conectar</div>
+
+        </div>
+      </main>
     </div>
   );
 }
