f806bcca325d05473b561b8856c1e0f596abb84d
HabacucMX <habacuc.vera@gmail.com>
2022-06-16 19:47:45 -0500

    Connect button breakpoints

--- packages/react-app/src/views/HomeScreen.jsx
+++ packages/react-app/src/views/HomeScreen.jsx
@@ -1,17 +1,12 @@
 import React, { useState } from "react";
-import PC from "../assets/punkcities.png"
+
 
 export default function HomeScreen() {
 
 
   return (
-    <div className="HomeCon">
-      <main className="HomeCon2">
-        <div className="hero">
-          <div className="center"><img src={PC} class="logo" alt="Punk Cities"/></div>
-          <div className="HomeBT">Conectar</div>
-        </div>
-      </main>
+    <div>
+      {}
     </div>
   );
 }
