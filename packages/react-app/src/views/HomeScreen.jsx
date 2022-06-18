cb3be4a9124e9c5e225d890442770b0de9851924
HabacucMX <habacuc.vera@gmail.com>
2022-06-18 13:51:17 -0500

    Home screen with new game

--- packages/react-app/src/views/HomeScreen.jsx
+++ packages/react-app/src/views/HomeScreen.jsx
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
+import NG from "../assets/NewGame.svg"
 
 
 export default function HomeScreen() {
-
-
   return (
-    <div>
-      {}
+    <div class="HomeCon">
+      <div >
+        <div class="NewBT">
+          <img class="NewIMG" src={NG}/>
+          <div class="NewDialog">
+            Bienvenido extraño<br/>
+            ¿Quién eres y de que ciudad vienes?
+          </div>
+          <div class="center">
+            <a href="./NewGame"><div class="NewDialog2">Responder</div></a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
