72746ef64acc4bb031ab8efeb067cf7d1ce4358d
HabacucMX <habacuc.vera@gmail.com>
2022-06-18 23:42:50 -0500

    New game update

--- packages/react-app/src/views/NewGame.jsx
+++ packages/react-app/src/views/NewGame.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import NG from "../assets/NewGame.svg"
 
 export default function NewGame({ tx, writeContracts }) {
   const [name, setName] = useState("");
@@ -20,54 +21,28 @@ export default function NewGame({ tx, writeContracts }) {
 
   return (
     <div class="HomeDiv">
-      <div class="CityMenu">
-        <a class="CityBT" href="./NewPlace">
-          New Place
-          <img
-            src={"https://punkcities.mypinata.cloud/ipfs/QmYpNQUw9Pw48amwLnjjS7rDXRCB1xfo8DLZUJNnkYeQVo"}
-            class="homevan"
-          />
-        </a>
-        <a class="CityBT" href="./MyPlaces">
-          My places
-          <img
-            src={"https://punkcities.mypinata.cloud/ipfs/QmcbcgbhvpznC8zns7zRY5KKN1WvS1QQ7t1M3BaPjfUE9E"}
-            class="homevan"
-          />
-        </a>
-        <a class="CityBT" href="./CityPlaces">
-          My city places
-          <img
-            src={"https://punkcities.mypinata.cloud/ipfs/QmSm6Ec8xEBTEB6ATkVmPybw4VRLiapm9K9fxLLxthgvq4"}
-            class="homevan"
-          />
-        </a>
-        <a class="CityBT" type="submit" href="./debug">
-          🧙🏽 Wizard Mode (Hard){" "}
-          <img
-            src={"https://punkcities.mypinata.cloud/ipfs/QmREGJmweJGKqWHFM1oF8WnsgMc9gTSV8t4ZkFBk3aBsPx"}
-            class="homevan"
-          />
-        </a>
-      </div>
-      <div class="NewGame">
+      <div class="">
+        <img class="NewIMG" src={NG}/>
+        <div class="NewDialog">
+          Bienvenido extraño<br/>
+          ¿Quién eres y de que ciudad vienes?
+        </div>
         <div class="container">
-          <div class="NewPlayer"></div>
-          <div class="NewGame-title">New Game</div>
+          <div class="NewGame-title">Responder</div>
           <div class="inputs">
-            <label>Twitter profile URL</label>
-            <input type="text" onChange={handleNameChange} placeholder="https://twitter.com/YourAccount" />
-            <label>Current City or Hometown</label>
-            <input type="text" onChange={handleHometownChange} placeholder="Mexico City" />
-            <label>Your Country</label>
-            <input type="text" onChange={handleCountryChange} placeholder="Mexico" />
+            <label>Soy:</label>
+            <input type="text" onChange={handleNameChange} placeholder="Elige un nombre o apodo" />
+            <label>De la ciudad:</label>
+            <input type="text" onChange={handleHometownChange} placeholder="La ciudad en que vas a jugar" />
+            <label>En el país:</label>
+            <input type="text" onChange={handleCountryChange} placeholder="El país donde esta tu ciudad" />
           </div>
           <div
             class="CreateAcc"
             type="submit"
             onClick={() => tx(writeContracts.YourContract.registerUser(name, hometown, country))}
           >
-            Create Profile
+            Crear perfil
           </div>
         </div>
       </div>
