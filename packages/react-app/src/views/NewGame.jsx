8bb62d87ea46f5e686297bc6f63fe508723930fb
HabacucMX <habacuc.vera@gmail.com>
2022-06-19 16:45:08 -0500

    NewPlayer form simplification

--- packages/react-app/src/views/NewGame.jsx
+++ packages/react-app/src/views/NewGame.jsx
@@ -4,7 +4,7 @@ import NG from "../assets/NewGame.svg"
 export default function NewGame({ tx, writeContracts }) {
   const [name, setName] = useState("");
   const [hometown, setHometown] = useState("");
-  const [country, setCountry] = useState("");
+  {/*const [country, setCountry] = useState("");*/}
 
   const handleNameChange = e => {
     const name = e.target.value;
@@ -14,33 +14,38 @@ export default function NewGame({ tx, writeContracts }) {
     const hometown = e.target.value;
     setHometown(hometown);
   };
-  const handleCountryChange = e => {
+  {/*const handleCountryChange = e => {
     const country = e.target.value;
     setCountry(country);
-  };
+  };*/}
 
   return (
     <div class="HomeDiv">
       <div class="">
         <img class="NewIMG" src={NG}/>
         <div class="NewDialog">
-          Bienvenido extraño<br/>
-          ¿Quién eres y de que ciudad vienes?
+          Welcome stranger<br/>
+          Who are you and what city do you come from?
         </div>
         <div class="container">
-          <div class="NewGame-title">Responder</div>
           <div class="inputs">
-            <label>Soy:</label>
-            <input type="text" onChange={handleNameChange} placeholder="Elige un nombre o apodo" />
-            <label>De la ciudad:</label>
-            <input type="text" onChange={handleHometownChange} placeholder="La ciudad en que vas a jugar" />
-            <label>En el país:</label>
-            <input type="text" onChange={handleCountryChange} placeholder="El país donde esta tu ciudad" />
+            <label>I am:</label>
+            <input type="text" onChange={handleNameChange} placeholder="Choose a name or nickname" />
+            <label>From:</label>
+            <select type="text" onChange={handleHometownChange} placeholder="The city where you are going to play">
+              <option disabled selected>
+                The city where you i going to play...
+              </option>
+              <option>Querétaro</option>
+              <option>CDMX</option>
+              <option>Bogotá</option>
+              <option>Global Citizen</option>
+            </select>
           </div>
           <div
             class="CreateAcc"
             type="submit"
-            onClick={() => tx(writeContracts.YourContract.registerUser(name, hometown, country))}
+            onClick={() => tx(writeContracts.YourContract.registerUser(name, hometown))}
           >
             Crear perfil
           </div>
